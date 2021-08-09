c12cede1d3209b6d47ed95697e9b863ff37a0b0d
BuildTools <unconfigured@null.spigotmc.org>
2021-08-09 12:24:02 +0800

    一个测试文件2

--- src/main/java/org/doushabo/test/TestJava.java
+++ src/main/java/org/doushabo/test/TestJava.java
@@ -3,5 +3,7 @@ package org.doushabo.test;
 public class TestJava {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test2");
+
     }
 }
